f40038f254ad96ef6a614a43838ba6076a659891
unknown <hamzaseif1333@gmail.com>
2022-08-18 11:51:41 +0300

    removed static files

--- staticfiles/rental/js/script.js
+++
@@ -1,7 +0,0 @@
-const daysInput = document.getElementById('id_rental_days')
-if (daysInput){
-    daysInput.addEventListener('change',(event)=>{
-        let price = parseInt(document.getElementById('price').innerText.slice(1))
-        document.getElementById('totalprice').innerText=price*event.target.value
-    })
-}
